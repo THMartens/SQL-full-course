367a571b23b9dcabe62a31f1f1a468893aa86084
TiesMartens <87567401+TiesMartens@users.noreply.github.com>
2025-11-17 15:51:27 +0100

    Create Table Template

--- Create table template.sql
+++ Create table template.sql
@@ -1,5 +1,7 @@
-SELECT TOP (1000) [id]
-      ,[person_name]
-      ,[birth_date]
-      ,[phone]
-  FROM [MyDatabase].[dbo].[persons]
+CREATE TABLE persons (
+id INT NOT NULL,
+person_name VARCHAR(50) NOT NULL,
+birth_date DATE,
+phone VARCHAR(15) NOT NULL,
+CONSTRAINT pk_persons PRIMARY KEY (id)
+)
